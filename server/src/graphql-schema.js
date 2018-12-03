8a18982a8a5a4c8ecc294647ee8cd0ed2a65ba64
Clement Sirvente <clement.sirvente@gmail.com>
2018-12-03 11:50:50 +0100

    server

---
+++ server/src/graphql-schema.js
@@ -0,0 +1,76 @@
+export const typeDefs = `
+  type Movie {
+  movieId: ID!
+  title: String
+  year: Int
+  plot: String
+  poster: String
+  imdbRating: Float
+  genres: [String]
+  similar(first: Int=3, offset:Int=0): [Movie]
+}
+
+type Query {
+  moviesByTitle(subString: String!, first: Int=3, offset: Int=0): [Movie]
+}
+`;
+
+export const resolvers = {
+  Query: {
+    moviesByTitle: (root, args, context) => {
+      let session = context.driver.session();
+      let query = `MATCH (movie:Movie) WHERE movie.title CONTAINS $subString RETURN movie LIMIT $first;`;
+      return session
+        .run(query, args)
+        .then(result => {
+          return result.records.map(record => {
+            return record.get("movie").properties;
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  Movie: {
+    genres: (movie, _, context) => {
+      let session = context.driver.session();
+      let params = { movieId: movie.movieId };
+      let query = `MATCH(m:Movie)-[:IN_GENRE]->(g:Genre)
+      WHERE m.movieId = $movieId
+       RETURN g.name AS genre`;
+
+      return session
+        .run(query, params)
+        .then(result => {
+          return result.records.map(record => {
+            return record.get("genre");
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    similar: (movie, _, context) => {
+      let session = context.driver.session();
+      let params = { movieId: movie.movieId };
+      let query = `MATCH (m:Movie) WHERE m.movieId = $movieId
+      MATCH (m)-[:IN_GENRE]->(g:Genre)<-[:IN_GENRE]-(movie:Movie)
+      WITH m, movie, COUNT(*) AS genreOverlap
+      MATCH (m)<-[:RATED]-(:User)-[:RATED]->(movie:Movie)
+      WITH movie,genreOverlap, COUNT(*) AS userRatedScore
+      RETURN movie ORDER BY (0.9 * genreOverlap) + (0.1 * userRatedScore)  DESC LIMIT 3`;
+
+      return session
+        .run(query, params)
+        .then(result => {
+          return result.records.map(record => {
+            return record.get("movie").properties;
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
